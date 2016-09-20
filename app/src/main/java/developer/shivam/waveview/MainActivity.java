a1d804506968c1e6ca21adeec78915b1650992e8
Shivam Satija <dr.droid27@gmail.com>
2016-09-20 20:01:08 +0530

    Added seekbar for controlling wave

--- app/src/main/java/developer/shivam/waveview/MainActivity.java
+++ app/src/main/java/developer/shivam/waveview/MainActivity.java
@@ -4,22 +4,51 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.LinearInterpolator;
+import android.widget.SeekBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     private Context mContext = MainActivity.this;
     private WaveView view;
+    private SeekBar speedSeekBar, amplitudeSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        /**
-         * Initializing the wave and setting it
-         *  to the contentView of this activity
-         */
-        view = new WaveView(mContext);
-        setContentView(view);
+        view = (WaveView) findViewById(R.id.sample_wave_view);
+        speedSeekBar = (SeekBar) findViewById(R.id.seekBarSpeed);
+        speedSeekBar.setMax(10);
+        speedSeekBar.setOnSeekBarChangeListener(this);
+        amplitudeSeekBar = (SeekBar) findViewById(R.id.seekBarAmplitude);
+        amplitudeSeekBar.setMax(10);
+        amplitudeSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (seekBar.getId() == R.id.seekBarSpeed) {
+            if (fromUser) {
+                Log.d("Value", String.valueOf(progress));
+                view.setSpeed(progress);
+            }
+        } else if (seekBar.getId() == R.id.seekBarAmplitude) {
+            if (fromUser) {
+                view.setAmplitude(progress);
+            }
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
--- app/src/main/java/developer/shivam/waveview/WaveView.java
+++ app/src/main/java/developer/shivam/waveview/WaveView.java
@@ -7,6 +7,7 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class WaveView extends View {
@@ -23,7 +24,7 @@ public class WaveView extends View {
     private Paint lineColor;
     private Paint secondLineColor;
     private Paint thirdLineColor;
-    int frequency = 90;
+    int frequency = 180;
     int amplitude = 80;
     private float shift = 0;
     private float movement2 = 0;
@@ -32,6 +33,7 @@ public class WaveView extends View {
     long time = System.currentTimeMillis();
     Path mPath = new Path();
     Path secondPath = new Path();
+    private float speed = (float) 0.5;
 
     public WaveView(Context context) {
         super(context);
@@ -49,12 +51,12 @@ public class WaveView extends View {
         lineColor = new Paint();
         lineColor.setAntiAlias(true);
         lineColor.setStrokeWidth(2);
-        lineColor.setColor(Color.parseColor("#F06292"));
+        lineColor.setColor(Color.parseColor("#64B5F6"));
 
         secondLineColor = new Paint();
         secondLineColor.setAntiAlias(true);
         secondLineColor.setStrokeWidth(2);
-        secondLineColor.setColor(Color.parseColor("#E91E63"));
+        secondLineColor.setColor(Color.parseColor("#2196F3"));
 
         handler1 = new Handler();
         handler1.postDelayed(new CustomRunnableOne(), 16);
@@ -64,7 +66,7 @@ public class WaveView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawColor(Color.parseColor("#F8BBD0"));
+        canvas.drawColor(Color.parseColor("#BBDEFB"));
         quadrant = getHeight()/3;
 
         width = canvas.getWidth();
@@ -74,11 +76,11 @@ public class WaveView extends View {
         mPath.lineTo(0, quadrant);
         secondPath.lineTo(0, quadrant*2);
 
-        for (int i = 0; i < width; i++) {
+        for (int i = 0; i < width + 10; i = i + 10) {
             x1 = (float) i;
 
-            y1 = quadrant + amplitude * (float) Math.sin(((i) * Math.PI / frequency) + shift);
-            y2 = quadrant * 2 + amplitude * (float) Math.sin(((i) * Math.PI / frequency) + shift);
+            y1 = quadrant + amplitude * (float) Math.sin(((i + 10) * Math.PI / frequency) + shift);
+            y2 = quadrant * 2 + amplitude * (float) Math.sin(((i + 10) * Math.PI / frequency) + shift);
 
             mPath.lineTo(x1, y1);
             secondPath.lineTo(x1, y2);
@@ -89,21 +91,34 @@ public class WaveView extends View {
         canvas.drawPath(secondPath, secondLineColor);
     }
 
-    public void setMovementOne(float movement) {
+    /*public void setMovementOne(float movement) {
         this.shift = movement;
         mPath.reset();
         secondPath.reset();
         invalidate();
-    }
+    }*/
 
 
     public class CustomRunnableOne implements Runnable {
 
         @Override
         public void run() {
-            float movement = (float) (shift + 0.2);
-            setMovementOne(movement);
+            mPath.reset();
+            secondPath.reset();
+            invalidate();
+            shift = shift + speed;
             handler1.postDelayed(new CustomRunnableOne(), 16);
         }
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed/8;
+        Log.d("speed", String.valueOf(speed) + " " + this.speed);
+        invalidate();
+    }
+
+    public void setAmplitude(int amplitude) {
+        this.amplitude = amplitude * 20;
+        invalidate();
     }
 }
